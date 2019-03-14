f4963fa59c2f0fb09791ef5e82bb5c7eb7dd3195
569895754 <569895754@qq.com>
2019-03-14 21:09:58 +0800

    test

--- src/global.js
+++ src/global.js
@@ -1,6 +1,8 @@
 import { Modal, message } from 'antd';
 import { formatMessage } from 'umi/locale';
 
+
+
 // Notify user if offline now
 window.addEventListener('sw.offline', () => {
   message.warning(formatMessage({ id: 'app.pwa.offline' }));
